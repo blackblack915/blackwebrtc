c54e705014127ef6db2b34e22dc0917f6080ea25
LuongLH <luonglh@smartosc.com>
2019-10-23 01:00:05 +0700

    Add

---
+++ BlackWebRTC/BlackWebRTC.h
@@ -0,0 +1,19 @@
+//
+//  BlackWebRTC.h
+//  BlackWebRTC
+//
+//  Created by Black on 10/22/19.
+//  Copyright © 2019 GoStream.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlackWebRTC.
+FOUNDATION_EXPORT double BlackWebRTCVersionNumber;
+
+//! Project version string for BlackWebRTC.
+FOUNDATION_EXPORT const unsigned char BlackWebRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlackWebRTC/PublicHeader.h>
+
+
